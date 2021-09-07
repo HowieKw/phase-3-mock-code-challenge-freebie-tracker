b3cf25cc4aae2795a1bcb7eeee0fba2e457bbc91
Howard Kwon <howie.ykwon70@gmail.com>
2021-09-07 18:18:06 -0400

    Practiced and Reviewed

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,23 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    # Dev#received_one?(item_name)
+        # accepts an item_name (string) and returns true if any of the freebies associated with the dev has that item_name,
+        # otherwise returns false
+    def received_one?(item_name)
+        !!self.freebies.find { |freebie| freebie.item_name == item_name }
+    end
+
+
+    # Dev#give_away(dev, freebie)
+    #     accepts a Dev instance and a Freebie instance, 
+    #     changes the freebie's dev to be the given dev; 
+    #     your code should only make the change if the freebie belongs to the dev who's giving it away
+    def give_away(dev, freebie)
+        if self == freebie.dev
+            freebie.update(dev_id: dev.id)
+        end
+    end
 
 end
